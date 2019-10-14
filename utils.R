2a6f710ee905f5c8787ced9319cd293796733b7b
Daniel Stahl <daniel@pop-os.localdomain>
2019-10-14 17:40:19 -0500

    improved charts and added nuance

--- utils.R
+++ utils.R
@@ -1,7 +1,9 @@
 get_q=function(rate, capital=0){
   (rate-capital)/(1+rate)
 }
-
+get_r=function(alpha, beta, capital=0){
+  ((alpha+beta)*capital+alpha)/beta
+}
 get_profit_differentiated=function(rates, alpha, beta, capital=0){
   q=get_q(rates, capital)
   (1+rates)*(beta/(alpha+beta))*pbeta(q, alpha, beta+1)/pbeta(q, alpha, beta)-1
