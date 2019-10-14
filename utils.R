3d342064abd22b0d842b53fb20d793cac2d395ad
Daniel Stahl <daniel@pop-os.localdomain>
2019-10-14 15:25:40 -0500

    added sections on partial equilibrium

---
+++ utils.R
@@ -0,0 +1,15 @@
+get_q=function(rate, capital=0){
+  (rate-capital)/(1+rate)
+}
+
+get_profit_differentiated=function(rates, alpha, beta, capital=0){
+  q=get_q(rates, capital)
+  (1+rates)*(beta/(alpha+beta))*pbeta(q, alpha, beta+1)/pbeta(q, alpha, beta)-1
+}
+get_profit_undifferentiated=function(rates, alpha, beta, capital=0){
+  q=get_q(rates, capital)
+  (1+rates)*(beta/(alpha+beta))*(1-pbeta(q, alpha, beta+1))/(1-pbeta(q, alpha, beta))-1
+}
+get_percent_differentiated=function(rates, alpha, beta, capital=0){
+  pbeta(get_q(rates, capital), alpha, beta)
+}
