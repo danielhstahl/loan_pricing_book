6511632822333d41d6e3b10cec06a48228dbc941
Daniel Stahl <daniel@pop-os.localdomain>
2019-10-19 07:53:20 -0500

    added minor updates

--- utils.R
+++ utils.R
@@ -15,3 +15,30 @@ get_profit_undifferentiated=function(rates, alpha, beta, capital=0){
 get_percent_differentiated=function(rates, alpha, beta, capital=0){
   pbeta(get_q(rates, capital), alpha, beta)
 }
+
+get_demand_adjusted_profit_differentiated=function(rates, alpha, beta, a, b){
+  ((1+rates)*(1-a*rates)*beta)/(alpha+beta)+a*rates-1-((b*alpha)/(alpha+beta))*((1+alpha)/(alpha+beta+1))
+}
+
+get_base_return=function(rates, x){
+  (1+rates)*(1-x)-1
+}
+
+demand_function=function(rates, x, a, b){
+  1-a*rates+b*x
+}
+
+get_optimal_r=function(alpha, beta, a, b){
+  optimize(
+    f=function(r){
+      -get_demand_adjusted_profit_differentiated(r, alpha, beta, a, b)
+    },
+    interval = c(0.0, 1.0)
+    #lower=0.0,
+    #upper=1.0
+  )
+}
+
+
+
+
